c7095f5212426352b34041bd22f1ff20fd4f49d3
zonghua <zh.h@outlook.com>
2016-09-19 21:24:19 +0800

    add check tables and auto create tables

--- src/main/java/com/honey/filter/ContextListener.java
+++ src/main/java/com/honey/filter/ContextListener.java
@@ -1,7 +1,12 @@
 package com.honey.filter;
 
 import com.honey.HoneyConstants;
+import com.honey.util.SqlUtil;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -11,13 +16,18 @@ import javax.servlet.ServletContextListener;
  * @date: 2015/8/17 9:24
  * Copyright © 2015/8/17 Shanghai Raxtone Software Co.,Ltd Allright Reserved
  */
-public class ContextListener implements ServletContextListener  {
+public class ContextListener implements ServletContextListener {
 
     private final String ctx = "_host";
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ContextListener.class);
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         sce.getServletContext().setAttribute(ctx, HoneyConstants.WEB_DOMAIN);
+
+        ServletContext application = sce.getServletContext();
+        ApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(application);
+        SqlUtil.checkTables(applicationContext, logger, "mysql.sql");
     }
 
     @Override
